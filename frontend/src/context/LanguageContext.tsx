336c587402f04199b1dd89fbeec69efb10a5cf8a
unknown <klinh9201@gmail.com>
2026-08-22 00:03:24 +0700

    Fix lang

--- frontend/src/context/LanguageContext.tsx
+++ frontend/src/context/LanguageContext.tsx
@@ -1,5 +1,5 @@
-import React, { createContext, useContext, useEffect, useState, ReactNode } from 'react';
-
+import { createContext, useContext } from "react";
+import type { ReactNode } from "react";
 type Language = 'EN' | 'VI';
 
 interface LanguageContextProps {
